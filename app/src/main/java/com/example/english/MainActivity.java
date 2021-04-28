3416e47f4a27544ab82e1b7cb4d2ff47fb010f77
katherina0799 <katherina0799@gmail.com>
2021-04-28 11:13:25 +0300

    lab_5

--- app/src/main/java/com/example/english/MainActivity.java
+++ app/src/main/java/com/example/english/MainActivity.java
@@ -3,19 +3,71 @@ package com.example.english;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btn;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (ImageButton) findViewById(R.id.imageButton);
     }
+
+    public void onPopup(View view) {
+
+
+                        PopupMenu popup_menu = new PopupMenu(MainActivity.this, view);
+
+                        popup_menu.inflate(R.menu.popup);
+        popup_menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                                                        @Override
+                                                        public boolean onMenuItemClick(MenuItem item) {
+                                                            switch (item.getItemId()) {
+                                                                case R.id.item1:
+                                                                   btn.callOnClick();
+                                                                    return true;
+                                                                case R.id.item2:
+                                                                    LayoutInflater inflater = getLayoutInflater();
+                                                                    View layout = inflater.inflate(R.layout.toast,
+                                                                            (ViewGroup) findViewById(R.id.toast_layout_root));
+                                                                    ImageView image = (ImageView) layout.findViewById(R.id.image);
+                                                                    image.setImageResource(R.drawable.l1);
+                                                                    TextView text = (TextView) layout.findViewById(R.id.text);
+                                                                    text.setText("Toast із зображенням");
+                                                                    Toast toast = new Toast(getApplicationContext());
+                                                                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                                                                    toast.setDuration(Toast.LENGTH_LONG);
+                                                                    toast.setView(layout);
+                                                                    toast.show();
+                                                                    return true;
+                                                                case R.id.item3:
+                                                                    Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                                                                    startActivity(intent);
+                                                                     return true;
+                                                                default:
+                                                                    return false;
+                                                            }}});
+                        popup_menu.show();
+    }
+
     public void sendMessage(View view) {
         TextView textView = (TextView) findViewById(R.id.txt1);
         textView.setText("Дрищ К.В. ІПЗс-19-1 Інженерія програмного забезпечення" );
